e623484da9f137d2aec72580ba2ea20a7275b322
bohdan_khrystych <bullhardyua@gmail.com>
2023-02-13 10:13:18 -0800

    add deployments

---
+++ deployments/create.sh
@@ -0,0 +1,6 @@
+kubectl apply -f postgres-configmap.yaml 
+kubectl apply -f postgres-pvc-pv.yaml 
+kubectl apply -f postgres-deployment.yaml 
+kubectl apply -f postgres-service.yaml 
+kubectl apply -f deployment.yaml 
+kubectl apply -f service.yaml 
---
+++ deployments/delete.sh
@@ -0,0 +1,6 @@
+kubectl delete -f postgres-deployment.yaml 
+kubectl delete -f postgres-configmap.yaml 
+kubectl delete -f postgres-service.yaml 
+kubectl delete -f postgres-pvc-pv.yaml 
+kubectl delete -f deployment.yaml 
+kubectl delete -f service.yaml 
