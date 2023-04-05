e955db2b1aed8f83732e3bd054262a39f4f385ac
bohdan_khrystych <bullhardyua@gmail.com>
2023-04-05 15:18:53 -0500

    test1

--- deployments/create.sh
+++ deployments/create.sh
@@ -5,6 +5,3 @@ kubectl apply -f postgres-service.yaml
 kubectl apply -f backend-deployment.yaml 
 kubectl apply -f backend-service.yaml 
 kubectl apply -f ingress-api.yaml 
-kubectl apply -f frontend-deployment.yaml 
-kubectl apply -f frontend-service.yaml
-kubectl apply -f ingress-ui.yaml 
