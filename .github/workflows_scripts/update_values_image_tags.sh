8cafac7461183346c717766ad7e4a559eaa6e534
Bohdan Khrystych <114461@nelnet.net>
2023-05-03 12:19:04 -0500

    debug

--- .github/workflows_scripts/update_values_image_tags.sh
+++ .github/workflows_scripts/update_values_image_tags.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-# Paths to the YAML configuration files
-CONFIG_PATH="${GITHUB_WORKSPACE}/apps/self-hosted-runners/external-repository.yaml"
-VALUES_IMAGE_PATH="${GITHUB_WORKSPACE}/apps/self-hosted-runners/values.image.yaml"
-
-# Read the images, valuePaths, and tags from the external-repository.yaml file
-IMAGES=($(yq r "${CONFIG_PATH}" 'images[].name'))
-VALUE_PATHS=($(yq r "${CONFIG_PATH}" 'images[].valuePath'))
-TAGS=($(yq r "${CONFIG_PATH}" 'images[].tag'))
-
-# Loop through each image, update the values.image.yaml with the new tag according to the valuePath
-for i in "${!IMAGES[@]}"; do
-  VALUE_PATH="${VALUE_PATHS[$i]}"
-  TAG="${TAGS[$i]}"
-
-  # Update the values.image.yaml file with the new tag
-  yq w -i "${VALUES_IMAGE_PATH}" "${VALUE_PATH}" "${TAG}"
-done
+# Run the Python script to update the values.image.yaml file
+python3 ./.github/workflows_scripts/update_values_image_tags.py
 
 # Check if any changes were made
-if git diff --quiet "${VALUES_IMAGE_PATH}"; then
+if git diff --quiet "${GITHUB_WORKSPACE}/apps/self-hosted-runners/values.image.yaml"; then
   echo "No changes were made to the values.image.yaml file"
 else
   echo "The values.image.yaml file was updated with new tags"
